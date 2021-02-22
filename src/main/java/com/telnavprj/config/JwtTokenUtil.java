8636a179bd3c63e6dcabdd800456ab048be63de4
vipulmchaudhari <vipulmchaudhari@gmail.com>
2021-02-22 22:01:40 +1100

    04 Updating hardcoded token
    04 Updating hardcoded token

--- src/main/java/com/telnavprj/config/JwtTokenUtil.java
+++ src/main/java/com/telnavprj/config/JwtTokenUtil.java
@@ -21,7 +21,7 @@ public class JwtTokenUtil implements Serializable {
 	
 	public static final long JWT_TOKEN_VALIDITY = 5*60*60;
 
-	@Value("javainuse")
+	@Value("telenav")
 	private String secret;
 
 	public String getUsernameFromToken(String token) {
